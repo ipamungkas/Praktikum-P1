6039035cd00c5a5398637354d7c366e285720ae4
unknown <ipamungkas@yahoo.com>
2013-09-28 17:31:59 +0700

    commit04

---
+++ Sesi01/src/ATMyoannes.java
@@ -0,0 +1,14 @@
+//created by Yoannes
+
+
+package Latihan2.sesi1.bin;
+
+public class ATMyoannes
+{
+	public static void main (String [] data){
+		System.out.println("Nama             : Yoannes Bosco Pamungkas");
+		System.out.println("Alamat           : Jl. Boncel 36");
+		System.out.println("Nomor Kartu      : 4097 6621 3201 5713");
+		System.out.println("Nama Bank        : Bank Mandiri");
+	}
+}	
