cd738741a76659f68fbbf605a0175b1723c51ac7
unknown <ipamungkas@yahoo.com>
2013-09-28 16:54:52 +0700

    commit03

---
+++ Sesi01/src/BiodataIpam.java
@@ -0,0 +1,12 @@
+
+package Latihan1.sesi1.bin;
+
+public class BiodataIpam
+{
+	public static void main (String [] data){
+		System.out.println("Nama             : Yoannes Bosco Pamungkas");
+		System.out.println("Alamat           : Jl. Boncel 36");
+		System.out.println("Golongan Darah   : B");
+		System.out.println("Jenis Kelamin    : Laki-laki");
+	}
+}	
