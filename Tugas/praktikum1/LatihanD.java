7e523061d30454f300e104190ed169d2bbba6971
unknown <ipamungkas@yahoo.com>
2014-01-04 16:14:01 +0700

    commit10

---
+++ Tugas/praktikum1/LatihanD.java
@@ -0,0 +1,18 @@
+//E:\Kuliah\Pemograman1\Sesi02\src>
+//Javac -d bin src/01AnatomiAplikasi/latihanD.java
+//java -cp bin
+//Latihan Anatomi Alikasi Java
+//Classpath
+
+package Latihan03.Praktikum01.bin;
+
+public class LatihanD
+{
+	private Runnable thread = new Runnable()
+	{
+		public void run()
+		{
+			System.out.println("coba coba coba.... kalo bisa sukur kalo ga ya coba lagi!!");
+		}
+	};
+}
